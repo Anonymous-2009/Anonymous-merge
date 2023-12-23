4e9ffb8c01693a499f41f13cc1a9960c921575fb
Anonymous <krishnabag751@gmail.com>
2023-12-23 09:00:49 +0000

    destruction

--- merge/index.js
+++ merge/index.js
@@ -2,3 +2,10 @@
 console.log(" hello world ");
 console.log(" this is branch feature ");
 
+let obj = {
+  a:" Anonymous ", 
+  b:21, 
+  c:" Delhi "
+}
+let {a:name,b:age,c:place} = obj;
+console.log(name,age,place);
